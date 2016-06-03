03577e740e033f6a9976dd6344dff3fcc5accd87
FadeMind <fademind@gmail.com>
2016-06-03 14:06:09 +0200

    setup-ssh.bash: NOTES

--- dev/setup-ssh.bash
+++ dev/setup-ssh.bash
@@ -1,7 +1,12 @@
-#/bin/bash
+#!/usr/bin/env bash
+
+### WARNING Create first your OWN SSH key and setup Github 
+### Articles: 
+#   https://wiki.archlinux.org/index.php/SSH_keys
+#   https://help.github.com/articles/generating-an-ssh-key/
 
 # Install required packages
-sudo pacman -S openssh gnupg kwalletmanager kwallet kwalletcli pinentry ksshaskpass
+sudo pacman -S --needed openssh gnupg kwalletmanager kwallet kwalletcli pinentry ksshaskpass
 
 # Install systemd unit
 mkdir -p $HOME/.config/systemd/user
@@ -13,13 +18,14 @@ touch $HOME/.bash_profile
 echo 'export SSH_ASKPASS="/usr/bin/ksshaskpass"' >> $HOME/.bash_profile
 
 # Running ssh-agent after launch terminal
-touch $HOME/.bashrc
-echo 'eval `ssh-agent -s` > /dev/null && ssh-add $HOME/.ssh/id_rsa' >> $HOME/.bashrc
+if [ -e $HOME/.bashrc ] 
+then
+    echo 'eval `ssh-agent -s` > /dev/null && ssh-add $HOME/.ssh/id_rsa' >> $HOME/.bashrc && source $HOME/.bashrc
+fi
 
 # Add ssh config
 mkdir $HOME/.ssh; touch $HOME/.ssh/config
 echo 'IdentityFile ~/.ssh/id_rsa' >> $HOME/.ssh/config
-# Change LOGIN on your GitHub login
+# Change LOGIN value on your GitHub login
 echo 'User LOGIN' >> $HOME/.ssh/config
 echo 'ForwardAgent yes' >> $HOME/.ssh/config
-
