7c92022fd9bf177968972db0ed40361f71e17b76
FadeMind <fademind@gmail.com>
2016-06-03 13:16:37 +0200

    staff.bash

---
+++ staff.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# WARNING This script will download all repositories **PapirusDevelopmentTeam** via SSH.
+# ATTENTION Launch this script _ALWAYS_ before make any changes in local repos. 
+# ATTENTION This tool will automatically merge latest changes to the local repository OR just download it.
+
+SSH_ACCESS="git@github.com:PapirusDevelopmentTeam"
+REPO=(bomi-skin-papirus libreoffice-papirus-theme papirus-aurorae-theme papirus-color-scheme papirus-gtk-theme papirus-icon-theme-gtk papirus-icon-theme-gtk papirus-icon-theme-kde papirus-k3b-theme papirus-kmail-theme papirus-konsole-colorscheme papirus-look-and-feel papirus-plasma-theme papirus-qtcurve-theme papirus-sddm-theme papirus-smplayer-theme papirus-vlc-theme papirus-wallpapers papirus-yakuake-theme PKGBUILDs)
+
+for r in ${REPO[@]}
+do 
+    if [ ! -d "$r" ]; then
+        git clone $SSH_ACCESS/$r
+    else
+        echo "DIR $r exists"
+        cd $r && git pull && cd ..
+    fi
+done
